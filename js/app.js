e6524acc979bbf5267cacd5478e0a7973e72b6e4
Jason-Donmoyer <j.donmoyer523@gmail.com>
2020-09-08 17:49:24 -0400

    added hamburger menu

--- js/app.js
+++ js/app.js
@@ -22,26 +22,34 @@ const navLinks = [{
 ];
 
 const mainNavArea = document.querySelector('#main-nav-list');
-
-
+const nav = document.querySelector('nav');
 const mainContent = document.getElementById('main-content');
+const hamburgerMenu = document.getElementById('hamburger-menu');
 
 
 // Get content onto page
 
+// Loops through object array and creates content of page
+
 navLinks.forEach((item) => {
-  console.log(item.id);
+  console.log(typeof item.id);
   let newListItem = document.createElement('li');
   let listItemLink = document.createElement('a');
-  listItemLink.setAttribute('href', '#');
+  listItemLink.href = '#' + item.id;
+  console.log(listItemLink.href);
   listItemLink.innerHTML = item.title;
   console.log(listItemLink.id);
+
+  // Creates new nav link
 
   newListItem.appendChild(listItemLink);
   mainNavArea.appendChild(newListItem);
 
+  // Creates new content div
+
   let flexContainer = document.createElement('div');
   flexContainer.classList.add('flex-container');
+  flexContainer.setAttribute('id', item.id);
   let newContent = document.createElement('p');
   let newContentHeader = document.createElement('h2');
   newContentHeader.innerHTML = item.title;
@@ -49,4 +57,11 @@ navLinks.forEach((item) => {
   flexContainer.appendChild(newContentHeader);
   flexContainer.appendChild(newContent);
   mainContent.appendChild(flexContainer);
+});
+
+// Hamburger Menu Event Listener
+// Shows and hides nav menu on mobile devices
+
+hamburgerMenu.addEventListener('click', () => {
+  nav.style.display === 'flex' ? nav.style.display = 'none' : nav.style.display = 'flex';
 });
