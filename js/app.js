515f21237702456aac62c994462f21d305885a0a
Jason-Donmoyer <j.donmoyer523@gmail.com>
2020-09-09 18:21:36 -0400

    js refactor v-1

--- js/app.js
+++ js/app.js
@@ -31,23 +31,66 @@ const hamburgerMenu = document.getElementById('hamburger-menu');
 
 // Loops through object array and creates content of page
 
+
+
 navLinks.forEach((item) => {
-  console.log(typeof item.id);
-  let newListItem = document.createElement('li');
-  let listItemLink = document.createElement('a');
-  listItemLink.href = '#' + item.id;
-  console.log(listItemLink.href);
-  listItemLink.innerHTML = item.title;
-  console.log(listItemLink.id);
+  createNavLink(item);
+  createContent(item);
+
+  // console.log(typeof item.id);
+  // let newListItem = document.createElement('li');
+  // let listItemLink = document.createElement('a');
+  // listItemLink.href = '#' + item.id;
+  // console.log(listItemLink.href);
+  // listItemLink.innerHTML = item.title;
+  // console.log(listItemLink.id);
 
   // Creates new nav link
 
-  newListItem.appendChild(listItemLink);
-  mainNavArea.appendChild(newListItem);
+  // newListItem.appendChild(listItemLink);
+  // mainNavArea.appendChild(newListItem);
 
   // Creates new content div
 
 
+  // let flexContainer = document.createElement('div');
+  // flexContainer.classList.add('flex-container');
+  // flexContainer.setAttribute('id', item.id);
+  // let newContent = document.createElement('p');
+  // let newContentHeader = document.createElement('h2');
+  // newContentHeader.innerHTML = item.title;
+  // newContent.textContent = item.content;
+  // flexContainer.appendChild(newContentHeader);
+  // flexContainer.appendChild(newContent);
+  // mainContent.appendChild(flexContainer);
+});
+
+// Hamburger Menu Event Listener
+// Shows and hides nav menu on mobile devices
+
+hamburgerMenu.addEventListener('click', () => {
+  nav.style.display === 'flex' ? nav.style.display = 'none' : nav.style.display = 'flex';
+});
+
+// Adds tabindex to flex containers for active and focus states
+
+const flexContainers = document.querySelectorAll('.flex-container');
+
+for (let i = 0; i < flexContainers.length; i++) {
+  flexContainers[i].setAttribute('tabindex', i);
+}
+
+function createNavLink(item) {
+  let newListItem = document.createElement('li');
+  let listItemLink = document.createElement('a');
+  listItemLink.href = '#' + item.id;
+  listItemLink.innerHTML = item.title;
+
+  newListItem.appendChild(listItemLink);
+  mainNavArea.appendChild(newListItem);
+}
+
+function createContent(item) {
   let flexContainer = document.createElement('div');
   flexContainer.classList.add('flex-container');
   flexContainer.setAttribute('id', item.id);
@@ -58,17 +101,4 @@ navLinks.forEach((item) => {
   flexContainer.appendChild(newContentHeader);
   flexContainer.appendChild(newContent);
   mainContent.appendChild(flexContainer);
-});
-
-// Hamburger Menu Event Listener
-// Shows and hides nav menu on mobile devices
-
-hamburgerMenu.addEventListener('click', () => {
-  nav.style.display === 'flex' ? nav.style.display = 'none' : nav.style.display = 'flex';
-});
-
-const flexContainers = document.querySelectorAll('.flex-container');
-
-for (let i = 0; i < flexContainers.length; i++) {
-  flexContainers[i].setAttribute('tabindex', i);
 }
