c1487831f2277f4a46c82c9cbb0d192eb0bb9251
Jason-Donmoyer <j.donmoyer523@gmail.com>
2020-09-10 16:49:17 -0400

    added fonts and refactored js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // dummy content
-const navLinks = [{
+const navContent = [{
     id: 'section1',
     title: 'Section One',
     content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias placeat iusto a pariatur ut tempora cumque maiores quo neque facere. Hic at, reiciendis placeat esse neque voluptas quaerat architecto facere recusandae asperiores voluptate reprehenderit excepturi optio quas quisquam voluptatum animi dolorum dolores eligendi fuga vero rerum. Numquam debitis assumenda iusto facilis expedita, est sit? Debitis eaque aliquid unde, voluptatibus, tenetur eos voluptatem suscipit, sint officia accusamus veniam optio quis nihil. Sequi non, doloremque sed, laborum corrupti porro perspiciatis omnis impedit consequatur ab, dolorum ad? Hic corrupti pariatur minima ducimus nostrum. Maxime laudantium, voluptate quasi magni est voluptatem ipsa deleniti culpa aperiam architecto. Omnis, fuga harum. Magni aliquam sequi, molestias quam ipsam nam reprehenderit necessitatibus optio dolorem nemo debitis, blanditiis aut! Facere iure doloremque dignissimos mollitia aliquam magni enim eos similique magnam modi repellendus accusantium ipsa voluptatem fugit possimus itaque tempora accusamus veniam ipsam eaque, quisquam illo incidunt nemo odit! A?'
@@ -21,6 +21,8 @@ const navLinks = [{
   }
 ];
 
+// Variables
+
 const mainNavArea = document.querySelector('#main-nav-list');
 const nav = document.querySelector('nav');
 const mainContent = document.getElementById('main-content');
@@ -31,38 +33,9 @@ const hamburgerMenu = document.getElementById('hamburger-menu');
 
 // Loops through object array and creates content of page
 
-
-
-navLinks.forEach((item) => {
+navContent.forEach((item) => {
   createNavLink(item);
   createContent(item);
-
-  // console.log(typeof item.id);
-  // let newListItem = document.createElement('li');
-  // let listItemLink = document.createElement('a');
-  // listItemLink.href = '#' + item.id;
-  // console.log(listItemLink.href);
-  // listItemLink.innerHTML = item.title;
-  // console.log(listItemLink.id);
-
-  // Creates new nav link
-
-  // newListItem.appendChild(listItemLink);
-  // mainNavArea.appendChild(newListItem);
-
-  // Creates new content div
-
-
-  // let flexContainer = document.createElement('div');
-  // flexContainer.classList.add('flex-container');
-  // flexContainer.setAttribute('id', item.id);
-  // let newContent = document.createElement('p');
-  // let newContentHeader = document.createElement('h2');
-  // newContentHeader.innerHTML = item.title;
-  // newContent.textContent = item.content;
-  // flexContainer.appendChild(newContentHeader);
-  // flexContainer.appendChild(newContent);
-  // mainContent.appendChild(flexContainer);
 });
 
 // Hamburger Menu Event Listener
@@ -80,9 +53,13 @@ for (let i = 0; i < flexContainers.length; i++) {
   flexContainers[i].setAttribute('tabindex', i);
 }
 
+// FUNCTIONS
+
 function createNavLink(item) {
   let newListItem = document.createElement('li');
+  newListItem.classList.add('navItem');
   let listItemLink = document.createElement('a');
+  listItemLink.classList.add('navLink');
   listItemLink.href = '#' + item.id;
   listItemLink.innerHTML = item.title;
 
